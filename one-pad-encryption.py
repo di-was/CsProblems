8c59778c354f5e0091b261fc0f3e35c1769d087c
di-was <diwasdahal109@gmail.com>
2022-05-22 12:05:23 +0545

    class based one time pad

--- one-pad-encryption.py
+++ one-pad-encryption.py
@@ -1,13 +1,27 @@
 from secrets import token_bytes
 
-def random_key(length):
-    bytes = token_bytes(length)
-    return int.from_bytes(bytes, "big")
 
-def encrypt(original):
-    original_bytes = original.encode()
-    dummy = random_key(len(original_bytes))
-    original_key = int.from_bytes(original_bytes, 'big')
-    encrypted = original_key ^ dummy
-    return dummy, encrypted
+class OneTimePad(object):
+    def random_key(self, length):
+        bytes = token_bytes(length)
+        return int.from_bytes(bytes, "big")
 
+  
+
+class Text(OneTimePad):
+    def __init__(self, content):
+        self.content = content
+        self.key = 0
+        self.decrypted = 0
+
+    def encrypt(self):
+        self.content = self.content.encode()
+        self.key = super().random_key(len(self.content))
+        original_key = int.from_bytes(self.content, 'big')
+        self.content = original_key ^ self.key
+        return self.content
+
+    def decrypt(self):
+        self.decrypted = self.content ^ self.key
+        bytes = self.decrypted.to_bytes((self.decrypted.bit_length() + 7)   //8,    "big")
+        return bytes.decode()
